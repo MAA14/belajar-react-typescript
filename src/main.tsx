9845c8685a3b68468c63be18add3dcda87f6bee7
MAA14 <muhammadahya252@gmail.com>
2024-06-19 20:26:32 +0700

    Update : add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -8,27 +8,42 @@ import { FormUserWithReducer } from "./basic-syntax/use-reducer-syntax/use-reduc
 import { UserContextProvider } from "./basic-syntax/use-context/UserContext.tsx";
 import { LoginWithUseContext } from "./basic-syntax/use-context/LoginWithUseContext.tsx";
 import { ToggleButton } from "./basic-syntax/use-ref-syntax/ToggleButton.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h3 className="center">Belajar Use State untuk Login Component</h3>
-    <Login></Login>
-    <h3 className="center">
-      Belajar Use State dengan Optional Value dan cara menggunakan Map untuk
-      membuat component JSX
-    </h3>
-    <Users></Users>
-    <h3>
-      Belajar Use State dengan empty statenya menggunakan {"{} as TypeData"}
-    </h3>
-    <IncDecWithState></IncDecWithState>
-    <h3>Belajar Use Reducer</h3>
-    <FormUserWithReducer></FormUserWithReducer>
-    <h3>Belajar Use Context</h3>
-    <UserContextProvider>
-      <LoginWithUseContext></LoginWithUseContext>
-    </UserContextProvider>
-    <h3>Belajar Use Ref</h3>
-    <ToggleButton></ToggleButton>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h3 className="center">
+                Belajar Use State untuk Login Component
+              </h3>
+              <Login></Login>
+              <h3 className="center">
+                Belajar Use State dengan Optional Value dan cara menggunakan Map
+                untuk membuat component JSX
+              </h3>
+              <Users></Users>
+              <h3>
+                Belajar Use State dengan empty statenya menggunakan{" "}
+                {"{} as TypeData"}
+              </h3>
+              <IncDecWithState></IncDecWithState>
+              <h3>Belajar Use Reducer</h3>
+              <FormUserWithReducer></FormUserWithReducer>
+              <h3>Belajar Use Context</h3>
+              <UserContextProvider>
+                <LoginWithUseContext></LoginWithUseContext>
+              </UserContextProvider>
+              <h3>Belajar Use Ref</h3>
+              <ToggleButton></ToggleButton>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
